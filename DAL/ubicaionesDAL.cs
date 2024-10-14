abaf5f39b8ac3e477e3b88f5f7ce986e07e83bbe
Martin3316 <5323100289@correcaminos.uts.edu.mx>
2024-10-13 21:54:22 -0700

    CRUD Ubicaciones hasta el boton agregar

---
+++ DAL/ubicaionesDAL.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using BLL;
+
+namespace DAL
+{
+    public class ubicaionesDAL
+    {
+        SQLDBHelper oConexion;
+        //Inicializar la conexion con la BD (Constructor)
+        public ubicaionesDAL() 
+        {
+          oConexion = new SQLDBHelper();
+        }
+        public bool Agregar(ubicaciones_BLL OubicacionesBLL)
+        {
+            SqlCommand cmdComando = new SqlCommand();
+
+            cmdComando.CommandText = "INSERT INTO Direcciones (Ubicacion, Latitud, Longitud) VALUES (@Ubicacion, @Latitud, @Longitud)";
+            cmdComando.Parameters.Add("@Ubicacion",SqlDbType.VarChar).Value=OubicacionesBLL.Ubicacion;
+            cmdComando.Parameters.Add("@Latitud", SqlDbType.VarChar).Value = OubicacionesBLL.Latitud;
+            cmdComando.Parameters.Add("@Longitud", SqlDbType.VarChar).Value = OubicacionesBLL.Longitud;
+
+            return oConexion.EjecutarComandoSQL(cmdComando);
+
+        }
+
+        public void Eliminar()
+        {
+
+        }
+
+        public void Modificar()
+        {
+
+        }
+        //Seleccionar los registros de la tabla mediante un SELECT
+        public DataTable Listar()
+        {
+            SqlCommand cmdComando = new SqlCommand();
+           //Sentencia SQL para traer todos los registros de la tabla "Direcciones"
+            cmdComando.CommandText = "SELECT * FROM Direcciones"; 
+            //Tipo de comando, ya sea texto, sp, etc.
+            cmdComando.CommandType = CommandType.Text;
+
+            DataTable TablaResultante = oConexion.EjecutarSentenciaSQL(cmdComando);
+
+            return TablaResultante;
+        }
+    }
+}
